1d02c8b1ea1a45c1915b9da9cf89322f5f2426ad
applecoleene <90641429+applecoleene@users.noreply.github.com>
2021-11-17 16:03:45 -0500

    inserted require passport variable

--- server/routes/survey-answer.js
+++ server/routes/survey-answer.js
@@ -3,6 +3,8 @@ let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
 
+let passport = require('passport');
+
 // connect to our survey answers Model
 let Survey = require('../models/survey');
 
