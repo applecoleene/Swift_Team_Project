b19e75f54318db4cae22a0a4f0d79bd539e17e0b
applecoleene <90641429+applecoleene@users.noreply.github.com>
2021-11-17 18:41:47 -0500

    added comment

--- server/routes/survey-answer.js
+++ server/routes/survey-answer.js
@@ -20,7 +20,7 @@ function requireAuth(req, res, next)
     next();
 }
 
-/* GET Route for the Book List page - READ Operation */
+/* GET Route for the Survey List page - READ Operation */
 router.get('/', surveyController.displaySurveyList);
 
 /* GET Route for displaying the Add page - CREATE Operation */
